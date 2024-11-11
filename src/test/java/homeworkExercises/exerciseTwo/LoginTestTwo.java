dd3c9a3567628a15b9483e4adc2bad17e4e800e2
Romina Caballero <rominacaballero8@gmail.com>
2024-11-11 14:41:01 -0300

    Added new changes

--- src/test/java/homeworkExercises/exerciseTwo/LoginTestTwo.java
+++ src/test/java/homeworkExercises/exerciseTwo/LoginTestTwo.java
@@ -10,31 +10,11 @@ import static org.junit.Assert.assertEquals;
 
 public class LoginTestTwo extends BaseTestTwo {
     @Test
-    public void testSuccessfulLoginAndCorrectListOfTransactions() {
-
-        driver.manage().window().maximize();
-
-        driver.get("https://demo.applitools.com/");
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-
-        LoginPageTwo loginPage = new LoginPageTwo(driver);
-        loginPage.setUsername("admin");
-        loginPage.setPassword("admin123");
-        loginPage.clickLogin();
-
+    public void testSuccessfulLogin() {
         HomePageTwo homePageTwo = new HomePageTwo(driver);
 
         Assert.assertEquals("https://demo.applitools.com/app.html", driver.getCurrentUrl());
         Assert.assertEquals("Jack Gomez", homePageTwo.getNameLocator());
 
-        List<String> expectedDescriptions = homePageTwo.getTransactionsDescription();
-        List<String> actualDescriptions = homePageTwo.getActualTransactionDescriptions();
-
-        assertEquals(expectedDescriptions, actualDescriptions);
-
-        var expectedNumberOfTransactions = homePageTwo.getNumberOfTransactions();
-
-        Assert.assertEquals(expectedNumberOfTransactions, homePageTwo.getNumberOfTransactions());
     }
 }
